5be6e54c76c2ee8ca1c3a7b421755ab2bfe6db87
HeTreeTerr <3110708879@qq.com>
2026-03-02 23:46:21 +0800

    基础pom整理

---
+++ quick-start/src/main/java/com/hss/springai/QuickStartApplication.java
@@ -0,0 +1,20 @@
+package com.hss.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Hss
+ * @date 2026-03-02
+ */
+@SpringBootApplication
+public class QuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class, args);
+    }
+}
