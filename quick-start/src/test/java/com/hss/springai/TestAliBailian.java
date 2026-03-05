c6d8a0b04564f41ce9d41538224c7c0ea40d63fd
HeTreeTerr <3110708879@qq.com>
2026-03-05 23:19:01 +0800

    测试阿里百炼模型的流式响应

--- quick-start/src/test/java/com/hss/springai/TestAliBailian.java
+++ quick-start/src/test/java/com/hss/springai/TestAliBailian.java
@@ -1,10 +1,16 @@
 package com.hss.springai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+
+import reactor.core.publisher.Flux;
 
 /**
  * 测试阿里百炼模型
@@ -20,5 +26,19 @@ public class TestAliBailian {
     public void testQwen(@Autowired DashScopeChatModel dashScopeChatModel){
         String content = dashScopeChatModel.call("你好，你是谁");
         System.out.println(content);
+    }
+
+    /**
+     * 流式响应
+     * @param dashScopeChatModel
+     */
+    @Test
+    public void testQwenStream(@Autowired DashScopeChatModel dashScopeChatModel){
+        
+        Flux<ChatResponse> stream = dashScopeChatModel.stream(new Prompt("你好，你是谁", DashScopeChatOptions
+            .builder()
+            .withModel(DashScopeApi.ChatModel.QWEN_PLUS.getModel())
+            .build()));
+        stream.toIterable().forEach(e -> System.out.println(e.getResult().getOutput().getText()));
     }
 }
