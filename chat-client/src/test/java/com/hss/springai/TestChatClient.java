7e233e0623dfe880c3e2cca0d353b099bf7bf0a6
HeTreeTerr <3110708879@qq.com>
2026-03-11 23:28:23 +0800

    chat-client: 在多套模型配置中，指定大模型

--- chat-client/src/test/java/com/hss/springai/TestChatClient.java
+++ chat-client/src/test/java/com/hss/springai/TestChatClient.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+
 import reactor.core.publisher.Flux;
 
 /**
@@ -15,11 +17,22 @@ public class TestChatClient {
     
     /**
      * 测试 ChatClient 基础使用
+     * 在多套模型配置中，指定大模型
      * @param chatClientBuilder
      */
-    @Test
+    /* @Test
     public void testChatClient(@Autowired ChatClient.Builder chatClientBuilder) {
         ChatClient chatClient = chatClientBuilder.build();
+        String response = chatClient.prompt()
+            .user("你好")
+            .call()
+            .content();
+        System.out.println(response);
+    } */
+    @Test
+    public void testChatClient(@Autowired DashScopeChatModel dashScopeChatModel) {
+        
+        ChatClient chatClient = ChatClient.builder(dashScopeChatModel).build();
         String response = chatClient.prompt()
             .user("你好")
             .call()
@@ -29,11 +42,21 @@ public class TestChatClient {
 
     /**
      * 测试 ChatClient 流式调用
+     * 在多套模型配置中，指定大模型
      * @param chatClientBuilder
      */
-    @Test
+    /* @Test
     public void testStreamChatClient(@Autowired ChatClient.Builder chatClientBuilder) {
         ChatClient chatClient = chatClientBuilder.build();
+        Flux<String> response = chatClient.prompt()
+            .user("你好")
+            .stream()
+            .content();
+        response.toIterable().forEach(System.out::println);
+    } */
+   @Test
+    public void testStreamChatClient(@Autowired DashScopeChatModel dashScopeChatModel) {
+        ChatClient chatClient = ChatClient.builder(dashScopeChatModel).build();
         Flux<String> response = chatClient.prompt()
             .user("你好")
             .stream()
