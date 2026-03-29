4ae86ee62b556c8ee4233c06cd56d27f87a68344
HeTreeTerr <3110708879@qq.com>
2026-03-29 16:00:43 +0800

    结构化输出

--- chat-client/src/test/java/com/hss/springai/TestStructuredOut.java
+++ chat-client/src/test/java/com/hss/springai/TestStructuredOut.java
@@ -22,6 +22,9 @@ public class TestStructuredOut {
                 .build();
     }
 
+    /**
+     * 测试布尔值输出
+     */
     @Test
     public void testBoolOut(){
         Boolean isComplain = chatClient.prompt()
@@ -41,5 +44,29 @@ public class TestStructuredOut {
             System.out.println("用户不是投诉，自动流转客服机器人。");
             //todo 继续调用 客服ChatClient 进行对话
         }
+    }
+
+    public record Address(
+        String name, // 收件人姓名
+        String phone, // 联系电话
+        String province, // 省
+        String city, // 市
+        String district, // 区/县
+        String detail // 详细地址
+    ){}
+
+    /**
+     * 测试实体输出
+     */
+    @Test
+    public void testEntityOut(){
+        Address address = chatClient.prompt()
+                    .system("""
+                    请从下条文本中提取收货信息
+                    """)
+                    .user("收货人：法外狂徒\n手机号：13800000000\n地址：浙江省杭州市西湖区文一西路100号8赚202室")
+                    .call()
+                    .entity(Address.class);
+        System.out.println(address);
     }
 }
